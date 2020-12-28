18a45758c827093edc6e592e6a159647e6000b95
kacper-cyra <kacper.cyranowski@gmail.com>
2020-12-28 23:36:41 +0100

    Rozstawienie poczatkowe + testy

--- src/pieces/king.js
+++ src/pieces/king.js
@@ -1,15 +1,40 @@
 import Piece from './piece';
 
 class King extends Piece {
-    constructor(x, y, side) {
+    constructor(x = -1, y = -1, side) {
         super(x, y, side);
+        this.validateInput(x,y,side)
+        
         this.name = 'king';
-        this.display = `<i class="fas fa-chess-king ${side}"></i>`; //fontawesome king
+        this.display = `<i class="fas fa-chess-king ${side}"></i>`;
+         //fontawesome king
+
+         if(side === "white"){
+            x !== -1 ? this.x = x : this.x = 7;
+            y !== -1 ? this.y = y : this.y = 4;
+         } else {
+            x !== -1 ? this.x = x : this.x = 0;
+            y !== -1 ? this.y = y : this.y = 4;
+         }
+
+
     }
     findLegalMoves() {
         const possibleMoves = [];
 
+        for(let x = -1; x <= 1; x++){
+            for(let y = -1; y <= 1; y++){
+                possibleMoves.push({x: this.x + x, y: this.y + y});
+            }
+        }
+
         return possibleMoves;
+    }
+
+    validateInput(x,y,side){
+        if(x < -1 || x > 7){ throw `x is out of range.`}
+        if(y < -1 || y > 7){ throw `y is out of range.`}
+        if(side !== "white" && side !== "black"){throw `Wrong value of parameter side: ${side}`}
     }
 }
 
