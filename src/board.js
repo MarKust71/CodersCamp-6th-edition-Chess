b78340f49d732dba67aecbd96a3924262030707d
LukiSacz <lukasz.p.saczawa@gmail.com>
2021-01-06 18:39:41 +0100

    Rook - possible moves

--- src/board.js
+++ src/board.js
@@ -1,13 +1,20 @@
 import Pawn from './pieces/pawn';
+import Rook from './pieces/rook';
 
 const board = new Array(8);
 for (let i = 0; i < 8; i++) {
     board[i] = new Array(8);
 }
 //mamy pustą tablice tu trzeba zaimportować figury wedle przykładu dla pionka
-let pawn = new Pawn(6, 0, 'white');
-board[pawn.x][pawn.y] = pawn;
-pawn = new Pawn(6, 1, 'white');
-board[pawn.x][pawn.y] = pawn;
+// let pawn = new Pawn(6, 0, 'white');
+// board[pawn.x][pawn.y] = pawn;
+// pawn = new Pawn(6, 1, 'white');
+// board[pawn.x][pawn.y] = pawn;
+
+//import wieży
+let rook = new Rook(7, 0, 'white');
+board[rook.x][rook.y] = rook;
+rook = new Rook(7, 7, 'white');
+board[rook.x][rook.y] = rook;
 
 export default board;
