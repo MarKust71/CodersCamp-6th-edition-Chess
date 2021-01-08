33e08ff783da2ca73364582c49562c2528f3b1ac
wengiel <radoslaw.kucia@gmail.com>
2021-01-08 18:35:50 +0100

    rozstawienie knighta

--- src/board.js
+++ src/board.js
@@ -1,3 +1,4 @@
+import Knight from './pieces/knight';
 import Pawn from './pieces/pawn';
 
 const board = new Array(8);
@@ -9,5 +10,14 @@ let pawn = new Pawn(6, 0, 'white');
 board[pawn.x][pawn.y] = pawn;
 pawn = new Pawn(6, 1, 'white');
 board[pawn.x][pawn.y] = pawn;
+
+//Knight - white - rozstawienie
+
+let knight = new Knight(7, 1, 'white');
+board[knight.x][knight.y] = knight;
+knight = new Knight(7, 6, 'white');
+board[knight.x][knight.y] = knight;
+
+// Knight - white - rozstawienie KONIEC
 
 export default board;
--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -8,7 +8,10 @@ class Knight extends Piece {
     }
     findLegalMoves() {
         const possibleMoves = [];
-
+            if (this.side == 'white') {
+                this.x - 2 > 0 this.y -1 && this.y +1 && possibleMoves.push(`${this.x - 2},${this.y - 1},${this.y + 1}`);
+                this.x -2
+            }
         return possibleMoves;
     }
 }
