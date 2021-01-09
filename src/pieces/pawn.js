cb4c7e7428384fd8c0f240c9a444b0849f6ecd38
Artur <arthur.gacek@gmail.com>
2021-01-09 08:46:53 +0100

    added pawn capture functionality

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -9,29 +9,39 @@ class Pawn extends Piece {
     }
 
     findLegalMoves() {
-        // const x = this.x;
-        // const y = this.y;
+        const x = this.x;
+        const y = this.y;
         const v = this.direction;
         const possibleMoves = [];
-        const enemy = this.pieceOnSquare(`${this.x + v}`, `${this.y}`);
-        const enemyByTwo = this.pieceOnSquare(`${this.x + v * 2}`, `${this.y}`);
-        const ownInFront = this.pieceOnSquare(`${this.x + v}`, `${this.y}`);
-        const firstMoveByTwo = `${this.x + v * 2},${this.y}`;
-        const firstMoveByOne = `${this.x + v},${this.y}`;
+        const enemy = this.pieceOnSquare(`${x + v}`, `${y}`);
+        const toCaptureOnLeft = this.pieceOnSquare(`${x + v}`, `${y - 1}`);
+        const toCaptureOnRight = this.pieceOnSquare(`${x + v}`, `${y + 1}`);
+        const enemyByTwo = this.pieceOnSquare(`${x + v * 2}`, `${y}`);
+        const ownInFront = this.pieceOnSquare(`${x + v}`, `${y}`);
+        const moveByTwo = `${x + v * 2},${y}`;
+        const moveByOne = `${x + v},${y}`;
+        const moveCrossLeft = `${x + v},${y - 1}`;
+        const moveCrossRight = `${x + v},${y + 1}`;
 
         if (!(enemy || ownInFront)) {
             if (this.x === (this.side === 'white' ? 6 : 1)) {
                 if (enemyByTwo) {
-                    possibleMoves.push(firstMoveByOne);
+                    possibleMoves.push(moveByOne);
                 } else {
-                    possibleMoves.push(firstMoveByOne);
-                    possibleMoves.push(firstMoveByTwo);
+                    possibleMoves.push(moveByOne);
+                    possibleMoves.push(moveByTwo);
                 }
             } else {
                 if (this.x !== (this.side === 'white' ? 0 : 7)) {
-                    possibleMoves.push(firstMoveByOne);
+                    possibleMoves.push(moveByOne);
                 }
             }
+        }
+        if (toCaptureOnRight && this.side !== toCaptureOnRight.side) {
+            possibleMoves.push(moveCrossRight);
+        }
+        if (toCaptureOnLeft && this.side !== toCaptureOnLeft.side) {
+            possibleMoves.push(moveCrossLeft);
         }
         return possibleMoves;
     }
