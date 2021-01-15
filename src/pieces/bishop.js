1bdd64ef9b808a2aab425382a04e49769e3be5d3
Marek Kustosz <markust71@hotmail.com>
2021-01-15 11:53:51 +0100

    eslint error fixed

--- src/pieces/bishop.js
+++ src/pieces/bishop.js
@@ -1,5 +1,5 @@
 import Piece from './piece';
-import board from '../board';
+// import board from '../board';
 
 class Bishop extends Piece {
     constructor(x, y, side) {
@@ -11,28 +11,27 @@ class Bishop extends Piece {
         const possibleMoves = [];
         console.log(`(X:${this.x}, Y:${this.y})`);
 
-        for (let i=1; i<=7; i++) {
-            if(this.x + i <= 7) {
+        for (let i = 1; i <= 7; i++) {
+            if (this.x + i <= 7) {
                 if (this.y + i <= 7) {
                     possibleMoves.push(`${this.x + i},${this.y + i}`);
                 }
                 if (this.y - i >= 0) {
-                    possibleMoves.push(`${this.x + i},${this.y - i}`)
-                }}
+                    possibleMoves.push(`${this.x + i},${this.y - i}`);
+                }
+            }
             if (this.x - i >= 0) {
                 if (this.y - i >= 0) {
-                    possibleMoves.push(`${this.x - i},${this.y - i}`)
+                    possibleMoves.push(`${this.x - i},${this.y - i}`);
                 }
                 if (this.y + i <= 7) {
-                    possibleMoves.push(`${this.x - i},${this.y + i}`)
+                    possibleMoves.push(`${this.x - i},${this.y + i}`);
                 }
             }
         }
         console.log(possibleMoves);
         return possibleMoves;
     }
-
-
 }
 
 export default Bishop;
