4a6c8d3b8dd962309f3da22de541c2df1ac6e37e
kacper-cyra <kacper.cyranowski@gmail.com>
2021-01-17 13:13:21 +0100

    removing unwanted changes

--- src/board.js
+++ src/board.js
@@ -19,14 +19,14 @@ export function boardSetup() {
         }
     }
 
-    // for (let i = 0; i < 8; i++) {
-    //     let pawn = new Pawn(6, i, 'white');
-    //     board[pawn.x][pawn.y] = pawn;
-    // }
-    // for (let i = 0; i < 8; i++) {
-    //     let pawn = new Pawn(1, i, 'black');
-    //     board[pawn.x][pawn.y] = pawn;
-    // }
+    for (let i = 0; i < 8; i++) {
+        let pawn = new Pawn(6, i, 'white');
+        board[pawn.x][pawn.y] = pawn;
+    }
+    for (let i = 0; i < 8; i++) {
+        let pawn = new Pawn(1, i, 'black');
+        board[pawn.x][pawn.y] = pawn;
+    }
 
     const kingWhite = new King(-1, -1, 'white');
     const kingBlack = new King(-1, -1, 'black');
@@ -43,30 +43,30 @@ export function boardSetup() {
     rookWhite = new Rook(7, 7, 'white');
     board[rookWhite.x][rookWhite.y] = rookWhite;
 
-    // let rookBlack = new Rook(0, 0, 'black');
-    // board[rookBlack.x][rookBlack.y] = rookBlack;
-    // rookBlack = new Rook(0, 7, 'black');
-    // board[rookBlack.x][rookBlack.y] = rookBlack;
+    let rookBlack = new Rook(0, 0, 'black');
+    board[rookBlack.x][rookBlack.y] = rookBlack;
+    rookBlack = new Rook(0, 7, 'black');
+    board[rookBlack.x][rookBlack.y] = rookBlack;
 
-    // let knightWhite = new Knight(7, 1, 'white');
-    // board[knightWhite.x][knightWhite.y] = knightWhite;
-    // knightWhite = new Knight(7, 6, 'white');
-    // board[knightWhite.x][knightWhite.y] = knightWhite;
+    let knightWhite = new Knight(7, 1, 'white');
+    board[knightWhite.x][knightWhite.y] = knightWhite;
+    knightWhite = new Knight(7, 6, 'white');
+    board[knightWhite.x][knightWhite.y] = knightWhite;
 
-    // let knightBlack = new Knight(0, 1, 'black');
-    // board[knightBlack.x][knightBlack.y] = knightBlack;
-    // knightBlack = new Knight(0, 6, 'black');
-    // board[knightBlack.x][knightBlack.y] = knightBlack;
+    let knightBlack = new Knight(0, 1, 'black');
+    board[knightBlack.x][knightBlack.y] = knightBlack;
+    knightBlack = new Knight(0, 6, 'black');
+    board[knightBlack.x][knightBlack.y] = knightBlack;
 
-    // let bishopBlack = new Bishop(0, 2, 'black');
-    // board[bishopBlack.x][bishopBlack.y] = bishopBlack;
-    // bishopBlack = new Bishop(0, 5, 'black');
-    // board[bishopBlack.x][bishopBlack.y] = bishopBlack;
+    let bishopBlack = new Bishop(0, 2, 'black');
+    board[bishopBlack.x][bishopBlack.y] = bishopBlack;
+    bishopBlack = new Bishop(0, 5, 'black');
+    board[bishopBlack.x][bishopBlack.y] = bishopBlack;
 
-    // let bishopWhite = new Bishop(7, 2, 'white');
-    // board[bishopWhite.x][bishopWhite.y] = bishopWhite;
-    // bishopWhite = new Bishop(7, 5, 'white');
-    // board[bishopWhite.x][bishopWhite.y] = bishopWhite;
+    let bishopWhite = new Bishop(7, 2, 'white');
+    board[bishopWhite.x][bishopWhite.y] = bishopWhite;
+    bishopWhite = new Bishop(7, 5, 'white');
+    board[bishopWhite.x][bishopWhite.y] = bishopWhite;
 }
 
 boardSetup();
