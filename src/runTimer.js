d4b844212b5b1cfe348c634dfe118d29f159d6bb
Artur <arthur.gacek@gmail.com>
2021-01-19 09:28:27 +0100

    add lost/win by end of time, disabled class on inactive timers

--- src/runTimer.js
+++ src/runTimer.js
@@ -1,4 +1,5 @@
 import { updatePlayerTimer } from './gameplaySidebar';
+import { winnerDialogBox } from './winnerDialogBox';
 
 export const runTimer = (function () {
     let runTimerSide;
@@ -15,14 +16,27 @@ export const runTimer = (function () {
 
     const startInterval = () => {
         if (runTimerSide === 'white') {
+            document.getElementById('whitePlayerTimer').classList.remove('disabledClock');
+            document.getElementById('blackPlayerTimer').classList.add('disabledClock');
+        } else {
+            document.getElementById('blackPlayerTimer').classList.remove('disabledClock');
+            document.getElementById('whitePlayerTimer').classList.add('disabledClock');
+        }
+        if (runTimerSide === 'white') {
             intervalId = setInterval(() => {
                 runTimerClockTimerWhite--;
                 updatePlayerTimer(document.getElementById('whitePlayerTimer'), runTimerClockTimerWhite);
+                if (runTimerClockTimerWhite < 0) {
+                    winnerDialogBox();
+                }
             }, 1000);
         } else {
             intervalId = setInterval(() => {
                 runTimerClockTimerBlack--;
                 updatePlayerTimer(document.getElementById('blackPlayerTimer'), runTimerClockTimerBlack);
+                if (runTimerClockTimerBlack < 0) {
+                    winnerDialogBox();
+                }
             }, 1000);
         }
     };
